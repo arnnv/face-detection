8534531950749697fdde7c81e35de605f27019ed
Arnav <arnnv.g@gmail.com>
2024-07-03 14:15:49 +0530

    Add face detection

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture('./videos/4540151-hd_1920_1080_30fps.mp4')
-
+cap = cv2.VideoCapture('videos/3.mp4')
 pTime = 0
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
 
 while True:
     success, img = cap.read()
+    img = cv2.resize(img, (1200, 720))
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+    # print(results)
+
+    if results.detections:
+        for i, detection in enumerate(results.detections):
+            # print(i, detection)
+            # mpDraw.draw_detection(img, detection)
+            bboxC = detection.location_data.relative_bounding_box
+            h, w, c = img.shape
+            bbox = int(bboxC.xmin * w), int(bboxC.ymin * h), int(bboxC.width * w), int(bboxC.height * h)
+            cv2.rectangle(img, bbox, (0, 255, 0), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1]-10),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
